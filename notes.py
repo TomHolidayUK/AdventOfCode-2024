ebdd5d2549fb8ddeef8fcbf80418468dc7dc3981
Tom Holiday <tom.holiday@mac.com>
2024-12-05 23:40:26 +0000

    cleanup

--- notes.py
+++
@@ -1,7 +0,0 @@
-# convert int char to int 
-char = 5
-int_representation = int(char)
-
-# sort array of ints
-int_array = [2,5,8,3,7]
-sorted_array = sorted(int_array)
--- python_notes.py
+++ python_notes.py
@@ -1,3 +1,11 @@
 # Find if a value is of a certain type
 value = "hello"
 isinstance(value, int)
+
+# convert int char to int 
+char = 5
+int_representation = int(char)
+
+# sort array of ints
+int_array = [2,5,8,3,7]
+sorted_array = sorted(int_array)
