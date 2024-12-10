0cc44da3f588c5d5b6dc3427105ff03a234226e1
Thomas Holiday <tholiday@guralp.local>
2024-12-10 11:09:35 +0000

    day 9 part 2 stuck

--- Day9/day9.py
+++ Day9/day9.py
@@ -1,4 +1,4 @@
-data_path = './day9_data_test.txt'
+data_path = './day9_data.txt'
 
 with open(data_path, 'r', encoding='utf-8') as file:
     file_content = file.read()
@@ -25,6 +25,8 @@ for i,char in enumerate(file_content):
             disk_list.append(".")
 
 print(disk_list)
+
+disk_list_copy = disk_list[:]
 
 # 3
 length = len(disk_list)
@@ -62,14 +64,87 @@ for i, num in enumerate(int_conversion):
 print("Part 1 Answer = ", part_1_total)
 
 # Part 2 Plan
-# 4 - let's represent the data differently, let's put it in a list where every odd index (which presents a space is alos a list)
-#     this list will consist of 2 parts: the amount of space left and a list of the values already added
-# 5 - then again loop backwards through the list and find the first possible space and fill it
-# 6 - perform checksum evaluation
+# 4 - we need to represent the data differently, let's use a dictionary 
+#     blocks will have a key "B" and spaces will have a key that represent's the amount of empty space left
+#     dictionaries are ordered so we can reverse iterate through them 
+# 5 - as we reverse iterate through we will find the earliest possible space for a block
+# 6 - if no suitable space exists nothing happens and we move on 
+# 7 - as we move blocks if empty space is left on the end, delete it
+# 8 - if we move a block that is not on the end we need to replace it with empty space and join blocks around it 
+# 9 - only move blocks to the left
 
 
-# 4 
-new_list = [[item, []] for item in file_content]
-print(new_list)
+# Part 2 Plan
+# use 2 pointers approach
 
-# 5 
+p1 = 0 
+p2 = len(disk_list_copy) - 1
+
+def find_required_space(list, size, block_start):
+    count = 0
+    for i in range(block_start):
+        if list[i] == ".":
+            count += 1
+        else:
+            count = 0
+        
+        if count == size:
+            return i - size + 1  
+    return -1
+
+# print(find_required_space(disk_list_copy, 2))
+
+print(disk_list_copy)
+
+while p2 >= 0:
+    #print("------")
+    if disk_list_copy[p2] == ".":
+        p2 -= 1
+        continue
+
+    #print(p2, disk_list_copy[p2])
+    # find block of this value
+    value = disk_list_copy[p2]
+    p3 = p2
+    while disk_list_copy[p3] == value:
+        p3 -= 1
+    block_size = p2 - p3
+
+    # so now we need to find the first available space for a block of size block_size
+    # this space also needs to be to the left of the block (p3)
+    space_index = find_required_space(disk_list_copy, block_size, p3 + 1)
+    if space_index == -1:
+        #print("no space found")
+        p2 -= block_size
+        continue
+
+    # now we have the space we need to do the move
+    #print("space found at ", space_index)
+    for space in range(space_index, space_index + block_size):
+        disk_list_copy[space] = value
+
+    # if block was at the end of the list, delete it 
+    if p2 == len(disk_list_copy) - 1:
+        #print("removing block from end")
+        disk_list_copy = disk_list_copy[:-block_size]
+        p2 -= (block_size - 1)
+    else:
+        # if it is in the middle, replace with spaces
+        #print("in middle, replacing with spaces")
+        for index in range(p2, p3, -1):
+            disk_list_copy[index] = "."
+        p2 -= 1
+
+    #print(disk_list_copy)
+    p2 -= 1
+
+
+part_2_total = 0 
+
+for i, num in enumerate(disk_list_copy):
+    if num != ".":
+        part_2_total += i * int(num)
+
+print("Part 2 Answer = ", part_2_total)
+
+# 6421738978405 - too high
