eb58641c2fbad59e7106004e44b664160e691444
Tom Holiday <tom.holiday@mac.com>
2024-12-13 00:24:38 +0000

    day 12 part 2 progress

--- Day12/day12.py
+++ Day12/day12.py
@@ -5,7 +5,6 @@ with open(data_path, 'r', encoding='utf-8') as file:
 
 grid = file_content.split()
 
-print(grid)
 
 # Plan
 # Write a function that finds the total area for a square
@@ -21,14 +20,18 @@ rows, cols = len(grid), len(grid[0])
 groups = {}
 in_group = set()
 
-def find_grid(y, x, visited):
+def find_group(y, x, visited):
         
         #print("current pos: ", x, y)
+
+        # avoid repetition
+        if (y,x) in in_group:
+            return
 
         groups[grid[y][x]].add((y,x))
         in_group.add((y,x))
               
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)] # Possible moves
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)] 
         
         for dy, dx in directions:
             new_y, new_x = y + dy, x + dx
@@ -37,16 +40,65 @@ def find_grid(y, x, visited):
                 visited[new_y][new_x] = True
                 
                 # Recurse to continue exploring
-                find_grid(new_y, new_x, visited)
+                find_group(new_y, new_x, visited)
 
                 # after exploring all possible paths from current cell mark as not visited so it can be included in other paths
                 # this essentially allows backtracking
                 visited[new_y][new_x] = False
 
 
-visited = [[False] * cols for _ in range(rows)]  # 2D array to track visited cells
+visited = [[False] * cols for _ in range(rows)]  
 visited[0][0] = True
 value = grid[0][0]
+
+def perimeter(group_identifier):
+    perimeter = 0 
+    for plot in groups[group_identifier]:
+        y, x = plot[0], plot[1]
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for dy, dx in directions:
+            new_y, new_x = y + dy, x + dx
+            if (new_y, new_x) not in groups[group_identifier]:
+                perimeter += 1
+    return perimeter
+
+def sides(group_identifier):
+
+    sides_total = 0
+
+    # find the top left most square, find it's top edge
+    group = list(groups[group_identifier])  
+    miny = group[0][0]  
+    minx = group[0][1]  
+
+    for plot in group:
+        if plot[0] < miny:
+            miny = plot[0]
+        if plot[1] < minx:
+            minx = plot[1]
+
+    # trace the edge of the perimeter, recording each time we change direction
+    current_location = [miny, minx]
+    print(current_location)
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)] # N, E, S, W
+    current_direction = 0
+    dy, dx = directions[current_direction][0], directions[current_direction][1]
+    new_y, new_x = y + dy, x + dx
+
+    if (new_y, new_x) not in groups[group_identifier]:
+        print("need to change direction")
+        current_direction += 1
+        if current_direction == 4:
+            current_direction = 0
+        
+
+
+
+
+
+    # go all the way to we reach the start position's top edge again
+
+total = 0
 
 for y, line in enumerate(grid):
     for x, char in enumerate(line):
@@ -54,5 +106,16 @@ for y, line in enumerate(grid):
             print("searching for ", y, x)
             value = grid[y][x]
             groups[value] = set()
-            find_grid(y, x, visited)
-            print(groups)
+            find_group(y, x, visited)
+            #print(groups)
+            total += len(groups[value]) * perimeter(value)
+            #print(total)
+
+print("Part 1 Total = ", total)
+
+value = grid[0][0]
+# find_grid(0, 0, visited)
+# print(groups)
+# print(perimeter(value))
+print(sides(value))
+
