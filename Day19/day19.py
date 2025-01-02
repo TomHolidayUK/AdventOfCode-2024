a7a948c5ac7589bf526d5581501407e4706c6a2f
Thomas Holiday <tholiday@guralp.local>
2025-01-02 10:06:34 +0000

    day 19 plan

---
+++ Day19/day19.py
@@ -0,0 +1,9 @@
+
+
+
+
+
+# Plan 
+# loop through desired designs
+# loop through colours of desired design and try to find in available patterns
+# if doesn't exist, increase size until found or until at the end of the desired design
