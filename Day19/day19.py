cd67ede9e9e74ca122fd4567ba9564584fd7e067
Tom <tom.holiday@mac.com>
2025-01-05 23:08:41 +0000

    day 19 prog

--- Day19/day19.py
+++ Day19/day19.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 data_path = './day19_data.txt'
 
 with open(data_path, 'r', encoding='utf-8') as file:
@@ -14,67 +16,142 @@ available_patterns, desired_patterns = file_content.split("\n\n")
 available_ptrns = available_patterns.split(", ")
 desired_ptrns = desired_patterns.split("\n")
 
-print(available_ptrns)
-print(desired_ptrns)
+# print(available_ptrns)
+# print(desired_ptrns)
 
-counter = 0
+# counter = 0
 
-def is_pattern_possible(pattern):
+# def is_pattern_possible(pattern):
+#     size = len(pattern)
+
+#     is_possible = True
+
+#     # we will use pointers to move through the disered pattern and compare to available patterns
+#     pos = 0
+
+#     while pos < size:
+#         char = (pattern[pos])
+
+#         if char in available_ptrns:
+#             #print(char, "available")
+#             pos += 1
+#         else:
+#             #print(char, "UNavailable")
+
+#             is_possible = False
+            
+#             left = pos
+#             right = size - pos - 1
+
+#             for i in range(left + 1):
+#                 for j in range(right + 1):
+#                     chars = pattern[pos - i : pos + j + 1]
+#                     #print(chars)
+#                     if chars in available_ptrns:
+#                         #print(chars, "available")
+#                         is_possible = True
+#                         # pos = pos + j + 1
+#                         pos += 1
+#                         break
+
+#             if is_possible == False:
+#                 #print("no possible combination to get", char)
+#                 break
+
+
+#     if is_possible:
+#         print(pattern, "is possible")
+#         return True
+#     else:
+#         print(pattern, "is not possible")
+#         return False
+
+
+
+import re
+
+def find_all_indices_regex(haystack, needle):
+    return [match.start() for match in re.finditer(re.escape(needle), haystack)]
+
+
+def is_pattern_possible2(pattern):
     size = len(pattern)
 
-    is_possible = True
+    possible = False
 
-    # we will use pointers to move through the disered pattern and compare to available patterns
-    pos = 0
+    char_suitablity = [False] * size
 
-    while pos < size:
-        char = (pattern[pos])
+    for ptrn in available_ptrns:
+        if all(char_suitablity):
+            print("at end - possible")
+            possible = True
+            break
+        indices = find_all_indices_regex(pattern, ptrn)
+        length = len(ptrn)
+        for index in reversed(indices):
+            print(ptrn, index)
+            for i in range(length):
+                char_suitablity[index + i] = True
+            print(char_suitablity)
 
-        if char in available_ptrns:
-            #print(char, "available")
-            pos += 1
-        else:
-            #print(char, "UNavailable")
+    # if not possible:
+    #     #print("not possible")
 
-            is_possible = False
-            
-            left = pos
-            right = size - pos - 1
-
-            for i in range(left + 1):
-                for j in range(right + 1):
-                    chars = pattern[pos - i : pos + j + 1]
-                    #print(chars)
-                    if chars in available_ptrns:
-                        #print(chars, "available")
-                        is_possible = True
-                        # pos = pos + j + 1
-                        pos += 1
-                        break
-
-            if is_possible == False:
-                #print("no possible combination to get", char)
-                break
-
-
-    if is_possible:
-        print(pattern, "is possible")
-        return True
-    else:
-        print(pattern, "is not possible")
-        return False
-
-
-# is_pattern_possible(desired_ptrns[4])
-
-
-for ptrn in desired_ptrns:
-    if is_pattern_possible(ptrn):
-        counter += 1
+    return possible
 
 
 
-print("Part 1 Solution = ", counter)
+
+    
+
+#is_pattern_possible2(desired_ptrns[0])
+print(is_pattern_possible2("uuwwwrgrbgrwrwgruuurbwbggwwgbbrwbbuwrrrwrgrgbbwbubrgrrg"))
 
 
-# 338 - too high
+# for ptrn in desired_ptrns:
+#     if is_pattern_possible2(ptrn):
+#         counter += 1
+
+
+# print("Part 1 Solution = ", counter)
+
+
+# # 338 - too high
+
+
+
+
+# available_patterns, desired_patterns = file_content.split("\n\n")
+
+# patterns = available_patterns.split(", ")
+# maxlen = max(map(len, patterns))
+# desired_ptrns = desired_patterns.split("\n")
+
+# @cache
+# def can_obtain(design):
+#     if design == "":
+#         return True
+#     for i in range(min(len(design), maxlen) + 1):
+#         if design[:i] in patterns and can_obtain(design[i:]):
+#             return True
+#     return False
+    
+# print(sum(1 if can_obtain(design) else 0 for design in desired_ptrns))
+
+# counter = 0 
+# counter2 = 0 
+
+# for design in desired_ptrns:
+#     # if can_obtain(design):
+#     #     counter += 1
+#     # if is_pattern_possible2(design):
+#     #     counter2 += 1
+#     if can_obtain(design) != is_pattern_possible2(design):
+#         print(design)
+
+# print(counter, counter2)
+
+
+
+# uuwwwrgrbgrwrwgruuurbwbggwwgbbrwbbuwrrrwrgrgbbwbubrgrrg
+# ugwwbbwwruguwbbuurbggubwwbrurrrubbwwgbbrrrwbgugwbwuubrggrg
